38f546ccf03f6d03fd2e7fb9bb6fdfc07ee4d17c
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2019-02-19 13:32:11 -0300

    Atualizado os sistema de ações no javaScript , o Script AgendaSite.Js esta funcionando perfeitamente, somente falta aplicar futuramente a parte de Excluir as Tarefas do Painel.

--- AgendaTarefas/AgendaSite.js
+++ AgendaTarefas/AgendaSite.js
@@ -1,31 +1,32 @@
 //Legacy
 var Id = 0;
 
+
 //Adicionar a nova tarefa a Dash Board//
 function CriarElemento(Descricao, TipoDeAnotacao)
     {
         var NewDiv=document.createElement('div');
         var position = document.getElementById('Escopo');
-        var newText = document.createTextNode(Descricao)
+        var newText = document.createTextNode(Descricao+Id)
 
-        NewDiv.id = Descricao;
+        NewDiv.id = Descricao+Id;
         NewDiv.classList.add("ListaTarefas_class_Medium");
         position.appendChild(NewDiv);
 
         NewDiv = document.createElement('ul');
-        position = document.getElementById(Descricao);
-        NewDiv.id = TipoDeAnotacao;
+        position = document.getElementById(Descricao+Id);
+        NewDiv.id = TipoDeAnotacao+Id;
         position.appendChild(NewDiv);
 
         NewDiv = document.createElement('li');
-        position = document.getElementById(TipoDeAnotacao);
+        position = document.getElementById(TipoDeAnotacao+Id);
         newText = document.createTextNode(Descricao);
         NewDiv.appendChild(newText);
         NewDiv.classList.add("Lista_Tarefas");
         position.appendChild(NewDiv);
 
         NewDiv = document.createElement('li');
-        position = document.getElementById(TipoDeAnotacao);
+        position = document.getElementById(TipoDeAnotacao+Id);
         newText = document.createTextNode(TipoDeAnotacao);
         NewDiv.appendChild(newText);
         NewDiv.classList.add("Lista_Tarefas_sobre");
@@ -37,12 +38,32 @@ function CriarElemento(Descricao, TipoDeAnotacao)
 //Ao clicar adicionar na Dashboard//
 document.querySelector("#NovaTarefa").addEventListener('click', function (e) {
     e.preventDefault();
+    const element = document.querySelector("#DialogoNovaTarefa")
     if (Id <= 2)
     {
-        CriarElemento("FelipeKatao/JavaScriptTest Add files via upload" + Id, "#1 opened on 3 Feb 2018 by FelipeKatao" + Id);
+        element.classList.remove("visibility_off")
+        element.classList.add("visibility_on");
     }
     else
     {
         alert("Limite de Tarefas estourdo, exclua tarefas antigas");
     }
+});
+
+document.querySelector("#EnviarBtNova").addEventListener('click', function (e) {
+    const element = document.querySelector("#DialogoNovaTarefa");
+    if (document.getElementById('TituloInput').value != 0)
+    {
+        CriarElemento("#"+Id+" "+document.getElementById('TituloInput').value, document.getElementById('DescrArea').value);
+        element.classList.add("visibility_off");
+        element.classList.remove("visibility_on");
+    }
+    else
+    {
+        alert("A tarefa precisa ter pelo menos um titulo!");
+    }
+});
+
+document.querySelector("#ClearTarefas").addEventListener('click', function (e) {
+    //Limpar as tarefas//
 });
