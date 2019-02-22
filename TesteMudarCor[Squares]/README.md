3183cfabb8df536eec639a3be60fb4d227c8fa23
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2019-02-22 02:07:56 -0300

    Exclusão de arquivos antigos e preparando para a versão de release

--- TesteMudarCor[Squares]/README.md
+++
@@ -1,2 +0,0 @@
-# JavaScriptTest
-Codigos com o basico de java script e js orientado a objetos 
--- TesteMudarCor[Squares]/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("#teste").addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log("Bird box");
-});
